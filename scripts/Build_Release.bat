926e01af02baa1bdc3c9402bb283509ab92db69b
ratkosrb <ratkomladic2@abv.bg>
2022-12-18 23:21:22 +0200

    Add build script for linux.

--- scripts/Build_Release.bat
+++
@@ -1,1 +0,0 @@
-dotnet publish ..\HermesProxy -p:PublishSingleFile=true -r win-x64 /p:Configuration=Release /p:platform="x64" --self-contained
---
+++ scripts/Build_Windows.bat
@@ -0,0 +1,1 @@
+dotnet publish ..\HermesProxy -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false -r win-x64 /p:Configuration=Release /p:platform="x64" --self-contained
